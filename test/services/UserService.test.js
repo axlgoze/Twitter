ee8298729d678ce4266822f24915b1d889f1f99c
Axel Reyes <rbaxel.f@gmail.com>
2022-04-27 14:21:38 -0500

    add test for 2nd requirement

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,7 +1,7 @@
 const UserService = require('./../../app/services/UserService')
 
 describe('Test for UserService Class', () =>{
-	test('Create  empty Object', () =>{
+	test('0) Create  empty Object', () =>{
 		//código a probar
 		const user = UserService.create('id','username','name')
 		//recibido--esperado
@@ -9,12 +9,22 @@ describe('Test for UserService Class', () =>{
 		expect(user.username).not.toBeUndefined()
 		expect(user.name).not.toBeUndefined()
 	});
-	test('2) Create a new user Using the UserService', () =>{
+	test('1) Create a new user Using the UserService', () =>{
 		const user = UserService.create(1,"axelReyes","Axel")
 		expect(user.id).toBe(1)
 		expect(user.username).toBe("axelReyes")
 		expect(user.name).toBe("Axel")
 		expect(user.bio).not.toBeUndefined()
+	});
+	test('2) getting a list of all values', () =>{
+		//primero se crea el obejto
+		const user = UserService.create(1,"JhonnyReyes","Jhon")
+		//alamacena lista del 'user' usando metodo getInfo()
+		const userInfoList = UserService.getInfo(user)
+		expect(userInfoList.[0]).toBe(1)
+		expect(userInfoList.[1]).toBe("JhonnyReyes")
+		expect(userInfoList.[2]).toBe("Jhon")
+		expect(userInfoList.[3]).toBe("Sin bio")
 	});
 })
 
