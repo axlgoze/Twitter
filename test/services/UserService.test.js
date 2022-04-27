79693dfadb2a5c29b953d7ea994496f05f7cc75f
Axel Reyes <rbaxel.f@gmail.com>
2022-04-27 14:09:34 -0500

    add create method to user instance

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,14 +3,14 @@ const UserService = require('./../../app/services/UserService')
 describe('Test for UserService Class', () =>{
 	test('Create  empty Object', () =>{
 		//código a probar
-		const user = new UserService('id','username','name')
+		const user = UserService.create('id','username','name')
 		//recibido--esperado
 		expect(user.id).not.toBeUndefined()
 		expect(user.username).not.toBeUndefined()
 		expect(user.name).not.toBeUndefined()
 	});
 	test('2) Create a new user Using the UserService', () =>{
-		const user = new UserService.create(1,"axelReyes","Axel")
+		const user = UserService.create(1,"axelReyes","Axel")
 		expect(user.id).toBe(1)
 		expect(user.username).toBe("axelReyes")
 		expect(user.name).toBe("Axel")
