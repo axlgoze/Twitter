c74c7de0dd10f1398d36f8f26f2f9d258242ba5a
Axel Reyes <rbaxel.f@gmail.com>
2022-04-27 13:18:05 -0500

    add constructor to by tested

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -2,7 +2,14 @@
 const User = require('./../models/user')
 
 class UserService{
-
+	constructor(id,username,name,bio){
+		this.id=id
+		this.username=username
+		this.name=name
+		this.bio=bio
+		this.dateCreated=new Date()
+		this.lastUpdated=new Date()
+	}
 }
 
 module.exports = UserService
