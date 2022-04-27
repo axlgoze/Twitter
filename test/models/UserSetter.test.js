7c8462da74864866fb79ee08386f7627856f8a5e
Axel Reyes <rbaxel.f@gmail.com>
2022-04-26 23:51:58 -0500

    fix: prueba para setters

--- test/models/UserSetter.test.js
+++ test/models/UserSetter.test.js
@@ -4,8 +4,8 @@ describe('Test for Setters', () =>{
 	test('4) Add setters', () =>{
 		const user = new User(1,"Axel Reyes", "Axel", "Bio")
 		user.setUsername = "Reyes"
-		expect(user.setUsername)toBe("Reyes")
+		expect(user.setUsername).toBe("Reyes")
 		user.setBio = "New Bio"
-		expect(user.setBio).to("New Bio")
+		expect(user.setBio).toBe("New Bio")
 	});
 })
