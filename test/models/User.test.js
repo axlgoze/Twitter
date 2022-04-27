5f3fdedab17e426d5b6a79c8d5d3ecca9e9df6c1
Axel Reyes <rbaxel.f@gmail.com>
2022-04-27 12:51:22 -0500

    Agregando otras pruebas a archivo unico

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -14,4 +14,22 @@ describe("Unit test for User class", () => {
 	   expect(user.dateCreated).not.toBeUndefined() //verifica que el valor no sea indefinido
 	   expect(user.lastUpdated).not.toBeUndefined()
 	});
+
+	//segunda prueba para clase User
+	test('2) Add getters', () =>{
+        const user = new User(1,"Axel Reyes", "Brayan", "Bio")
+        expect(user.getUsername).toBe("Axel Reyes")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+	});
+
+	//Tercera prueba
+	   test('3) Add setters', () =>{
+                const user = new User(1,"Axel Reyes", "Axel", "Bio")
+                user.setUsername = "Reyes"
+                expect(user.username).toBe("Reyes")
+                user.setBio = "New Bio"
+                expect(user.bio).toBe("New Bio")
+        });
 })
