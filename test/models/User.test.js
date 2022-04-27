8860aab109bef485e85c5585c02c2b7b21e84f7c
Axel Reyes <rbaxel.f@gmail.com>
2022-04-26 20:07:07 -0500

    add:test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,6 +2,16 @@ const User = require('./../app/models/user')
 
 describe("Unit test for User class", () => {
 	test('Create an User object', => {
-	
+	   //aqui va el código que usaras en tu app
+	   const user = new User(1,'Axl Reyes','Axel','Bio','dateCreated','lastUpdated');
+	   
+	   //results validaton
+	   //comparacion que iguala el valor izquierdo con el valor derecho (esperado)
+	   expect(user.id).toBe(1)
+	   expect(user.username).toBe("Axel Reyes")
+	   expect(user.name).toBe("Axel")
+	   expect(user.bio).toBe("Bio")
+	   expect(user.dateCreated).toBe("dateCreated")
+	   expect(user.lastUpdated).toBe("lastUpdated")
 	});
 })
